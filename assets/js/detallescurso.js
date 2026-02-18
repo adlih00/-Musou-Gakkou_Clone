d7f00e1a83885192efcbe466c45ff1af645c83dd
Elizabeth Alcantar <elizabeth_9700@hotmail.com>
2026-02-18 17:37:07 -0600

    fix: Se elimina init de mas en detalles

--- assets/js/detallescurso.js
+++ assets/js/detallescurso.js
@@ -79,7 +79,7 @@ async function init() {
             i++
     }
 }
-document.addEventListener('DOMContentLoaded', init); //Esperamos a que el DOM cargue antes de intetar manipularlo
+//document.addEventListener('DOMContentLoaded', init); //Esperamos a que el DOM cargue antes de intetar manipularlo
 
 // Funciones para desplegar mensaje al añadir al carrito
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
